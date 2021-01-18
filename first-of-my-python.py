1aefacfc06296a3e0c6e143d87aa1042a93ed906
DO-CO <77617196+DO-CO@users.noreply.github.com>
2021-01-18 12:25:01 +0100

    Create first-of-my-python.py

---
+++ first-of-my-python.py
@@ -0,0 +1,6 @@
+# #  PRINT "Hello World!" as a test 
+# Print function is used to print elements on the screen
+# Define a string x 
+# A string in Python is a sequence of characters
+# String in python are surrounded by single or double quotation marks
+print("I say 'Hello World !' " )
